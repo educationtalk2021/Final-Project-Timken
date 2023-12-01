988c33181dd6c60ee077a51f090ec5229837278a
Tapan <educationtalk2021@gmail.com>
2023-12-01 12:51:44 +0530

    Home Page Created !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { TestformComponent } from './testform/testform.component';
+import { FormsModule } from '@angular/forms';
+import { Test1Component } from './test1/test1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    TestformComponent,
+    Test1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/testform/testform.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testform',
+  templateUrl: './testform.component.html',
+  styleUrl: './testform.component.css'
+})
+export class TestformComponent {
+
+}
